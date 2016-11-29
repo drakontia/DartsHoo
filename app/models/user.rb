286643ec241de3ec26c6e58df9d56e15a846a5b7
drakontia <mhden@drakontia.com>
2016-11-29 18:45:28 +0900

    Add validation to user model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ActiveRecord::Base
   validates :dartslive_id, uniqueness: true
   validates :email, presence: true, uniqueness: true
   validates :password_digest, presence: true
+  validates_presence_of :password, :password_confirmation
+  validates_confirmation_of :password
 end
