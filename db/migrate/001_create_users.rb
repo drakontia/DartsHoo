6c047a6e2b6714cdd03acb2b25f2c77249bf2f20
drakontia <mhden@drakontia.com>
2016-11-29 18:45:08 +0900

    Use naming rule of ActiveRecord.

--- db/migrate/001_create_users.rb
+++ db/migrate/001_create_users.rb
@@ -2,11 +2,12 @@ class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
       t.string   :user_name, null: false
-      t.integer  :dartsliveid
-      t.integer  :rating
-      t.integer  :cricketstats
+      t.integer  :dartslive_id
+      t.string   :rating
+      t.integer  :ratingnum
+      t.integer  :statscricket
       t.integer  :stats01
-      t.datetime :registered_at
+      t.datetime :created_at
       t.datetime :updated_at
       t.string   :email, null: false
       t.string   :password_digest, null: false
