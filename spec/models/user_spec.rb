60917764dac348e431ef110d6bce989e2c323752
drakontia <mhden@drakontia.com>
2016-12-02 14:15:19 +0900

    Add user model test.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 describe "User" do
-  
+  let(:user) { create(:user, user_name: 'John Doe', password: 'password', password_confirmation: 'password') }
+
+  it "is created." do
+    expect(user.user_name).to eq('John Doe')
+  end
+
+  it "has rating and rank by stats." do
+    expect(user.stats01).to eq(80)
+    expect(user.statscricket).to eq(2.5)
+    expect(user.rating01).to eq(10)
+    expect(user.ratingcricket).to eq(8)
+    expect(user.ratingnum).to eq(9)
+    expect(user.rating).to eq('BB')
+  end
+
+  it "update his stats." do
+    user.update(stats01: 87, statscricket: 2.76)
+    expect(user.rating01).to be_within(0.1).of(11.4)
+    expect(user.ratingcricket).to be_within(0.1).of(9.3)
+    expect(user.ratingnum).to be_within(0.1).of(10.3)
+    expect(user.rating).to eq('A')
+  end
 end
