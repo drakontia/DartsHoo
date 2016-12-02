6346c3c8c207a872cf076dd763ace16405af5ca7
drakontia <mhden@drakontia.com>
2016-12-02 14:14:56 +0900

    Remove stats of 01 & cricket. Because, it is calculatable.

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,12 +1,12 @@
 FactoryGirl.define do
   factory :user do
-    name "MyString"
-    dartsliveid 1
-    rating 1
-    cricketstats 1
-    stats01 1
-    registered_at "2016-11-24"
+    user_name 'John Doe'
+    dartslive_id 123456781234
+    stats01 80
+    statscricket 2.5
+    created_at "2016-11-24"
     updated_at "2016-11-24"
+    email 'john@example.com'
     password_digest 'password'
   end
 end
