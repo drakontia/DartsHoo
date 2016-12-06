4d7cbd4e212e6d61d981556416d56e41d7101010
drakontia <mhden@drakontia.com>
2016-12-06 18:30:41 +0900

    Add reg01 model function

--- app/models/reg.rb
+++ app/models/reg.rb
@@ -1,2 +1,16 @@
-class Reg < ApplicationRecord
+module Reg
+  extend ActiveSupport::Concern
+
+  included do
+    belongs_to :user, optional: true
+    belongs_to :game, optional: true
+    has_many :rounds, -> { order('roundno asc') }
+
+    attr_accessor :gametype, :regno, :round_count
+  end
+
+  def regend?
+    self.regno > 15
+  end
+
 end
--- app/models/reg01.rb
+++ app/models/reg01.rb
@@ -1,2 +1,22 @@
-class Reg01 < Reg
+class Reg01 < ApplicationRecord
+  include Reg
+
+  attr_accessor :regno, :gametype, :number01
+
+  def total_point
+    self.rounds.inject(0) { |total, round|
+      total + round.total_score
+    }
+  end
+
+  def gamestats
+    n = 0
+    totals = self.rounds.inject(0) do |total, round|
+      total + round.total_score unless total[0] > ( self.number01 * 0.8 )
+      n += 1 unless total > ( self.number01 * 0.8 )
+      [total, n]
+    end
+    totals.sort { |t, n| ( t / n ).round(0) }
+  end
+
 end
