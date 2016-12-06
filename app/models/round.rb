9a56c1f55cb5ab25671822015b87bdd781d43c36
drakontia <mhden@drakontia.com>
2016-12-06 18:30:19 +0900

    Refactoring round model

--- app/models/round.rb
+++ app/models/round.rb
@@ -1,29 +1,34 @@
 class Round < ApplicationRecord
   belongs_to :reg01, optional: true
-  belongs_to :regcricket, optional: true
+
+  attr_accessor :roundno, :shot1st, :shot2nd, :shot3rd
 
   def total_score
-    total_score = score(self.shot1st).to_i + score(self.shot2nd).to_i + score(self.shot3rd).to_i
-    total_score
+    score(self.shot1st) + score(self.shot2nd) + score(self.shot3rd)
   end
 
   def score(shot)
-    if shot == 'OUT' then
+    if shot == 'OUT' or shot == 'NONE' then
       score = 0
     elsif shot == 'BULL' or shot == 'DBULL' then
       score = 50
     elsif shot == 'SBULL' then
       score = 25
     else
-      case shot[2, 1]
-      when "S" then
-        score = shot[0, 2].to_i
-      when "D" then
-        score = shot[0, 2].to_i * 2
-      when "T" then
-        score = shot[0, 2].to_i * 3
-      end
+      score = multi(shot)
     end
     score
   end
+
+  def multi(shot)
+    case shot[2, 1]
+    when "S" then
+      shot[0, 2].to_i
+    when "D" then
+      shot[0, 2].to_i * 2
+    when "T" then
+      shot[0, 2].to_i * 3
+    end
+  end
+
 end
