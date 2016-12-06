817c6a56549f29a7a4b5b7a51e29252a4bcf93b8
drakontia <mhden@drakontia.com>
2016-12-06 18:26:49 +0900

    Use 12 digits card number

--- spec/factories/cards.rb
+++ spec/factories/cards.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :card do
     sequence(:card_name) { |n| "user#{n}" }
-    sequence(:card_number) { |n| "#{n * 1111}" }
+    sequence(:card_number) { |n| "#{n * 111111111111}" }
     user
     #card_name "user1"
     #card_number 111122223333
