7589ff4ee2ce4b3198c825a8502111fdfc3d02d9
drakontia <mhden@drakontia.com>
2016-12-07 18:38:56 +0900

    Add variable pattern for test.

--- spec/factories/rounds.rb
+++ spec/factories/rounds.rb
@@ -1,27 +1,36 @@
 FactoryGirl.define do
   factory :round do
     sequence(:roundno)
-    shot1st "BULL"
-    shot2nd "05D"
-    shot3rd "05S"
     reg01
 
-    factory :round_just do
-      roundno 10
+    factory :round_70 do
+      sequence(:roundno)
       shot1st "BULL"
-      shot2nd "07S"
-      shot3rd "07D"
+      shot2nd "05D"
+      shot3rd "10S"
+    end
+
+    factory :round_hat do
+      sequence(:roundno)
+      shot1st "BULL"
+      shot2nd "BULL"
+      shot3rd "BULL"
+    end
+
+    factory :round_just do
+      roundno 5
+      shot1st "BULL"
+      shot2nd "19S"
+      shot3rd "16D"
     end
 
     factory :round_burst do
-      roundno 10
+      roundno 5
       shot1st "BULL"
-      shot2nd "20T"
-      shot3rd "NONE"
     end
 
     factory :round_under do
-      roundno 10
+      roundno 5
       shot1st "BULL"
       shot2nd "OUT"
       shot3rd "OUT"
