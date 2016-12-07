d0be7e88a021615372ed34171a6ba8bf9b93d61a
drakontia <mhden@drakontia.com>
2016-12-07 18:39:51 +0900

    Add score method check test.

--- spec/models/round_spec.rb
+++ spec/models/round_spec.rb
@@ -2,9 +2,18 @@ require 'rails_helper'
 
 describe "Round" do
   let(:reg) { create(:reg01) }
-  let(:round) { create(:round, reg01: reg) }
+  let(:round) { create(:round_70, reg01: reg) }
+
+  it "calculate score" do
+    expect(Round.score("10S")).to eq(10)
+    expect(Round.score("09D")).to eq(18)
+    expect(Round.score("16T")).to eq(48)
+    expect(Round.score("BULL")).to eq(50)
+    expect(Round.score("NONE")).to eq(0)
+  end
 
   it "shows total score of itself" do
-    expect(round.total_score).to eq(65)
+    expect(round.persisted?).to be_truthy
+    expect(round.total_score).to eq(70)
   end
 end
