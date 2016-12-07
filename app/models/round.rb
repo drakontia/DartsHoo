52ede0f4dd880b895de043fe14d957881d8aefec
drakontia <mhden@drakontia.com>
2016-12-07 18:38:33 +0900

    Change method from instance to class for easy check.

--- app/models/round.rb
+++ app/models/round.rb
@@ -4,30 +4,32 @@ class Round < ApplicationRecord
   attr_accessor :roundno, :shot1st, :shot2nd, :shot3rd
 
   def total_score
-    score(self.shot1st) + score(self.shot2nd) + score(self.shot3rd)
+    Round.score(@shot1st) + Round.score(@shot2nd) + Round.score(@shot3rd)
   end
 
-  def score(shot)
-    if shot == 'OUT' or shot == 'NONE' then
-      score = 0
-    elsif shot == 'BULL' or shot == 'DBULL' then
-      score = 50
-    elsif shot == 'SBULL' then
-      score = 25
-    else
-      score = multi(shot)
+  class << self
+    def score(shot)
+      if shot == 'OUT' or shot == 'NONE'
+        score = 0
+      elsif shot == 'BULL' or shot == 'DBULL'
+        score = 50
+      elsif shot == 'SBULL'
+        score = 25
+      else
+        score = multi(shot)
+      end
+      score
     end
-    score
-  end
 
-  def multi(shot)
-    case shot[2, 1]
-    when "S" then
-      shot[0, 2].to_i
-    when "D" then
-      shot[0, 2].to_i * 2
-    when "T" then
-      shot[0, 2].to_i * 3
+    def multi(shot)
+      case shot[2, 1]
+      when "S"
+        shot[0, 2].to_i
+      when "D"
+        shot[0, 2].to_i * 2
+      when "T"
+        shot[0, 2].to_i * 3
+      end
     end
   end
 
