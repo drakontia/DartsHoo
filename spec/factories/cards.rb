b691ec92ea39539a4e38ef173981393e38109d21
drakontia <mhden@drakontia.com>
2017-01-06 16:34:45 +0900

    Cleanup old column

--- spec/factories/cards.rb
+++ spec/factories/cards.rb
@@ -2,8 +2,8 @@ FactoryGirl.define do
   factory :card do
     sequence(:card_name) { |n| "user#{n}" }
     sequence(:card_number) { |n| "#{n * 111111111111}" }
+    stats01 80
+    statscricket 2.5
     user
-    #card_name "user1"
-    #card_number 111122223333
   end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,11 +1,8 @@
 FactoryGirl.define do
   factory :user do
     user_name 'John Doe'
-    stats01 80
-    statscricket 2.5
-    created_at "2016-11-24"
-    updated_at "2016-11-24"
     email 'john@example.com'
     password_digest 'password'
+    password_confirmation 'password'
   end
 end
