d5b1d0e27a136104c355e902c43c807acf655596
drakontia <mhden@drakontia.com>
2017-01-06 16:36:00 +0900

    Change instance.

--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -5,19 +5,19 @@ describe "Card" do
   let(:card) { create(:card, user: user) }
 
   it "has rating and rank by stats." do
-    expect(user.stats01).to eq(80)
-    expect(user.statscricket).to eq(2.5)
-    expect(user.rating01).to eq(10)
-    expect(user.ratingcricket).to eq(8)
-    expect(user.ratingnum).to eq(9)
-    expect(user.rating).to eq('BB')
+    expect(card.stats01).to eq(80)
+    expect(card.statscricket).to eq(2.5)
+    expect(card.rating01).to eq(10)
+    expect(card.ratingcricket).to eq(8)
+    expect(card.ratingnum).to eq(9)
+    expect(card.rating).to eq('BB')
   end
 
   it "update own stats." do
-    user.update(stats01: 87, statscricket: 2.76)
-    expect(user.rating01).to be_within(0.1).of(11.4)
-    expect(user.ratingcricket).to be_within(0.1).of(9.3)
-    expect(user.ratingnum).to be_within(0.1).of(10.3)
-    expect(user.rating).to eq('A')
+    card.update(stats01: 87, statscricket: 2.76)
+    expect(card.rating01).to be_within(0.1).of(11.4)
+    expect(card.ratingcricket).to be_within(0.1).of(9.3)
+    expect(card.ratingnum).to be_within(0.1).of(10.3)
+    expect(card.rating).to eq('A')
   end
 end
